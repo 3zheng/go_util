7cc11dd7c9e604999ddee26990f301380b6d2c31
3zheng <914509007@qq.com>
2024-04-15 18:19:49 -0400

    改进质数算法，使用平方根作为截止数

--- primeNum.go
+++ primeNum.go
@@ -1,17 +1,26 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // 显示输入数字为止的所有质数
 func ShowPrimeNumber(finalNum int) {
+	if finalNum <= 1 {
+		fmt.Println("输入数字不是有效数字", finalNum)
+	}
+
 	var primeNums []int
 	primeNums = append(primeNums, 2) //2是质数
 	for i := 3; i <= finalNum; i++ { //从3开始判断
+		endNum := int(math.Sqrt(float64(i))) //一个合数的最小质因数一定小于等于该合数的平方根
 		for _, elem := range primeNums {
 			if i%elem == 0 { //能被整除，不是质数
 				break
 			}
-			if elem >= i/2 { //当循环到i的一半时仍然是质数，那么这个数就是质数
+			//if elem >= i/2 { //当循环到i的一半时仍然是质数，那么这个数就是质数
+			if elem >= endNum {
 				primeNums = append(primeNums, i)
 				break
 			}
@@ -20,8 +29,37 @@ func ShowPrimeNumber(finalNum int) {
 
 	fmt.Println("primeNums capacity is ", cap(primeNums))
 	fmt.Printf("从1到%d的质数共有%d个，列表如下：\n", finalNum, len(primeNums)) //打印质数
-	for _, elem := range primeNums {
+	/*for _, elem := range primeNums {
 		fmt.Printf("%d, ", elem)
+	}*/
+
+}
+
+func ShowPrimeNumberV01(finalNum int) {
+	if finalNum <= 1 {
+		fmt.Println("输入数字不是有效数字", finalNum)
 	}
+
+	var primeNums []int
+	primeNums = append(primeNums, 2) //2是质数
+	for i := 3; i <= finalNum; i++ { //从3开始判断
+		//endNum := int(math.Sqrt(float64(i))) //一个合数的最小质因数一定小于等于该合数的平方根
+		endNum := i / 2 //当循环到i的一半时仍然是质数，那么这个数就是质数
+		for _, elem := range primeNums {
+			if i%elem == 0 { //能被整除，不是质数
+				break
+			}
+			if elem >= endNum {
+				primeNums = append(primeNums, i)
+				break
+			}
+		}
+	}
+
+	fmt.Println("primeNums capacity is ", cap(primeNums))
+	fmt.Printf("从1到%d的质数共有%d个，列表如下：\n", finalNum, len(primeNums)) //打印质数
+	/*for _, elem := range primeNums {
+		fmt.Printf("%d, ", elem)
+	}*/
 
 }
---
+++ primeNum_test.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestShowPrimeNumberV01(t *testing.T) {
+	before := time.Now().UnixMilli() //UnixMilli毫秒 UnixMicro微秒 UnixNano纳秒
+	ShowPrimeNumberV01(900000)
+	after := time.Now().UnixMilli()
+	t.Logf("1/2数算法耗时：%E ns\n", float64(after-before))
+	fmt.Printf("\n1/2数算法耗时：%E ms\n", float64(after-before))
+}
+
+func TestShowPrimeNumber(t *testing.T) {
+	before := time.Now().UnixMilli()
+	ShowPrimeNumber(900000)
+	after := time.Now().UnixMilli()
+	t.Logf("平方根算法耗时：%E ns\n", float64(after-before))
+	fmt.Printf("\n平方根算法耗时：%E ms\n", float64(after-before))
+}
